2d55bb4a8163a09afcdb270f07c5fa225a3d038d
Nikita Ryanov <ryanov.nikita@gmail.com>
2026-03-29 16:49:55 +0300

    update

--- src/test/java/io/debezium/postgres2lake/infrastucture/s3/S3IcebergEventSaverTest.java
+++ src/test/java/io/debezium/postgres2lake/infrastucture/s3/S3IcebergEventSaverTest.java
@@ -22,7 +22,6 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.time.Duration;
-import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
