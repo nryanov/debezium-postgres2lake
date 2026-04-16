20a55307a85433dfb65540748ad36e5ff5c2b008
Nikita Ryanov <ryanov.nikita@gmail.com>
2026-04-17 02:41:30 +0300

    refactor: final polishing

--- modules/iceberg/src/test/java/io/debezium/postgres2lake/iceberg/infrastructure/S3IcebergNessieCatalogTest.java
+++ modules/iceberg/src/test/java/io/debezium/postgres2lake/iceberg/infrastructure/S3IcebergNessieCatalogTest.java
@@ -1,4 +1,4 @@
-package io.debezium.postgres2lake.iceberg.infrastructure.s3;
+package io.debezium.postgres2lake.iceberg.infrastructure;
 
 import io.debezium.postgres2lake.domain.EventSaver;
 import io.debezium.postgres2lake.iceberg.infrastructure.profile.IcebergOutputFormatProfile;
